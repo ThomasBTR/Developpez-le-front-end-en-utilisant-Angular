81249926465a3422270a820c79482c40c6ae7a5a
Thomas BTR <thomatito971@gmail.com>
2023-01-20 15:38:58 +0100

    start working with service and observables

--- src/app/components/olympic/olympic.component.ts
+++ src/app/components/olympic/olympic.component.ts
@@ -2,23 +2,33 @@ import {Component, OnInit} from '@angular/core';
 import {MockOlympics} from "../../mock-olympics";
 import {Olympic} from "../../core/models/Olympic";
 import {Participation} from "../../core/models/Participation";
+import {OlympicService} from "../../core/services/olympic.service";
 
 @Component({
   selector: 'app-olympic',
   templateUrl: './olympic.component.html',
   styleUrls: ['./olympic.component.scss']
 })
-export class OlympicComponent{
+export class OlympicComponent implements OnInit{
 
-  olympics : Olympic[] = MockOlympics;
+  olympics : Olympic[] = [];
 
   selectedOlympic?: Olympic;
   participations?: Participation[];
+
+  constructor(private olympicService : OlympicService) {
+  }
+
+  ngOnInit(): void {
+    this.olympicService.getOlympics();
+  }
 
   onSelect(olympic: Olympic) {
     this.selectedOlympic = olympic;
     this.participations = olympic.participations;
   }
+
+
 
 
 }
